6ad7e6457f537d4c90fafadacd7258c7661c5e01
Go Watanabe <gowatana@vmtn.jp>
2024-02-09 02:08:50 +0900

    update

---
+++ labs/02_base-vsphere/infra-dc-01_infra-cluster-04.ps1
@@ -0,0 +1,7 @@
+# Base vSphere info
+$base_dc_name = "infra-dc-01"
+$base_cluster_name = "infra-cluster-04"
+#$base_rp_name = "rp-lab-01"
+$base_hv_name = "infra-esxi-09.go-lab.jp"
+$base_ds_name = "ds-vmfs-infra-esxi-09" # Clone Target Datastore
+$base_pg_name = "Nested-Trunk-Network"
